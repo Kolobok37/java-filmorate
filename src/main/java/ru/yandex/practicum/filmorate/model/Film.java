68dc7c3afe3be42e0c401d57d31f336059872522
Aleksandr <kolobok373711@yandex.ru>
2023-08-06 22:01:58 +0300

    f

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.time.Duration;
 import java.time.LocalDate;
-import java.util.Optional;
-import java.util.OptionalInt;
 
 @Data
 @AllArgsConstructor
